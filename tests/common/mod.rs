b2415460bfe6c1e8d8efce24e509a5ef35f6e40a
Antonio Yang <yanganto@gmail.com>
2020-12-03 11:50:43 +0800

    implment get object

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -6,27 +6,30 @@ use subprocess::{Exec, Popen};
 #[cfg(feature = "ci-test")]
 use tokio::time::{delay_for, Duration};
 
-pub struct Server {
+pub struct MockServer {
     inner: Popen,
     // TODO: use tempfile crate
     pub tmp_file: String,
+    pub tmp_file_content: String,
     pub access_key: &'static str,
     pub secret_key: &'static str,
 }
 
-impl Server {
+impl MockServer {
     pub async fn new() -> Self {
         let tmp_file_name = "/tmp/s3cs-test";
         let access_key = "AAAAAAAAAAAAAAAAAAAA";
         let secret_key = "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS";
         let popen = Exec::cmd(executable_path("s3cs")).popen().unwrap();
+        let tmp_file_content = "This is a test file\n";
 
         // Eventurally, the file base key is for admin
         let mut file = File::create(format!("/tmp/{}", access_key)).unwrap();
         file.write_all(secret_key.as_bytes()).unwrap();
 
         let mut tmp_file = File::create(tmp_file_name).unwrap();
-        tmp_file.write_all(b"This is a test file\n").unwrap();
+        let content: Vec<u8> = tmp_file_content.bytes().collect();
+        tmp_file.write_all(&content).unwrap();
 
         #[cfg(feature = "ci-test")]
         delay_for(Duration::from_secs(30)).await;
@@ -34,13 +37,14 @@ impl Server {
         Self {
             inner: popen,
             tmp_file: tmp_file_name.into(),
+            tmp_file_content: tmp_file_content.into(),
             access_key,
             secret_key,
         }
     }
 }
 
-impl Drop for Server {
+impl Drop for MockServer {
     fn drop(&mut self) {
         self.inner.kill().unwrap();
         self.inner.wait().unwrap();
